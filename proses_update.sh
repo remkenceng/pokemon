46f2528e97ae324e916976e321b596964c913ca1
remkenceng <rapiariep@gmail.com>
2025-03-30 11:50:07 +0700

    first commit

--- proses_update.sh
+++ proses_update.sh
@@ -1,53 +1,41 @@
 #!/bin/bash
-clear
-fun_bar() {
-    CMD[0]="$1"
-    CMD[1]="$2"
-    (
-        [[ -e $HOME/fim ]] && rm $HOME/fim
-        ${CMD[0]} -y >/dev/null 2>&1
-        ${CMD[1]} -y >/dev/null 2>&1
-        touch $HOME/fim
-    ) >/dev/null 2>&1 &
-    tput civis
-    while true; do
-        for ((i = 0; i < 18; i++)); do
-            echo -ne "\033[0;32m#"
-            sleep 0.1s
-        done
-        [[ -e $HOME/fim ]] && rm $HOME/fim && break
-        echo -e "\033[0;33m]"
-        sleep 1s
-        tput cuu1
-        tput dl1
-    done
-    tput cnorm
-}
 
-cleanup() {
+## BERSIHKAN TAMPILAN
+tput reset
+
+proses_membersihkan() {
     rm -rf /usr/local/sbin/*
 }
-res1() {
-    cleanup
-    wget https://raw.githubusercontent.com/remkenceng/pokemon/main/menu/menu.zip
-    unzip menu.zip
-    rm -rf menu.zip
-    chmod +x *
-    mv * /usr/local/sbin
+# res1() {
+#     cleanup
+#     wget https://raw.githubusercontent.com/remkenceng/pokemon/main/menu/menu.zip
+#     unzip menu.zip
+#     rm -rf menu.zip
+#     chmod +x *
+#     mv * /usr/local/sbin
+# }
+
+proses_unduh() {
+    nama_nama_filenya=(
+        add-bot-notif addhost addss addssh addtr addvless addws autokill autoreboot
+        backup bot bw ceklim cekss cekssh cektr cekvless cekws clearcache clearlog
+        del-bot-notif delexp delss delssh deltr delvless delws fixcert hapus-bot
+        limit-ip-ssh limitspeed lock m-bot m-noob m-sshws m-ssws m-trial m-trojan
+        m-vless m-vmess mbot-backup mbot-panel member member-ws menu menu-backup
+        menu-x prot regis renewss renewssh renewtr renewvless renewws reset
+        restart restart-bot restore run sd speedtest stop-bot tendang trial trialss
+        trialtr trialvless trialws tunnel unlock xp z9dtrial
+    )
+    for files in "${nama_nama_filenya[@]}"; do
+        wget "https://raw.githubusercontent.com/remkenceng/pokemon/main/menu/$files"
+    done
 }
-function noobzvpn() {
-wget "${REPO}/noobzvpns.zip"
-unzip noobzvpns.zip
-bash install.sh
-rm noobzvpns.zip
-systemctl restart noobzvpns
-print_success "NOOBZVPN"
-}
+
 netfilter-persistent
 clear
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
 fun_bar 'res1'
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
 echo -e ""
-read -n 1 -s -r -p "Press [ Enter ] to Back on Menu : "
+read -n 1 -s -r -p "Tekan [ Enter ] Untuk Kembali : "
 menu
