5bfe63b39ca21f890d9842191d61c2fa520218e3
remkenceng <rapiariep@gmail.com>
2025-03-31 15:46:52 +0700

    first commit

--- pokemon.sh
+++ pokemon.sh
@@ -1,56 +1,88 @@
 #!/bin/bash
 
-reset
+reset_screen() {
+    reset
+    echo -e "\033[1;32m============================================\033[0m"
+    echo -e "\033[1;33m             Pokemon Tunneling             \033[0m"
+    echo -e "\033[1;32m============================================\033[0m"
+}
+
+reset_screen
 
 CEK_IP=$(curl -sS ipv4.icanhazip.com)
-REPO_IZIN_IP=$(curl -sS https://raw.githubusercontent.com/remkenceng/pokemon/main/izin/ip | awk '{print $3}')
+REPO_IZIN_IP=$(curl -sS https://raw.githubusercontent.com/remkenceng/pokemon/main/izin/ip)
 
-if [[ $REPO_IZIN_IP != *$CEK_IP* ]]; then
-    echo "Lu Belum Berlangganan !"
-    exit 1
-fi
+error_msg() {
+    reset_screen
+    echo -e "\033[1;31mIp [$CEK_IP] Tidak Terdaftar Pada Database Pokemon\033[0m"
+    echo ""
+    echo -e "  \033[1;32m Contact WhatsApp :\033[0m https://wa.me/6282124807605"
+    echo -e "  \033[1;32m Contact Telegram :\033[0m @RemKenceng"
+    echo ""
+    read -n 1 -s -r -p $'\033[1;37mPress [Enter] : \033[0m'
+    bash pokemon.sh
+}
 
-echo "1. Install Pokemon Tunneling"
-echo "2. Update Pokemon Tunneling"
-echo "3. Update Dependency"
-echo "4. Get Root"
-echo "5. Exit"
-echo ""
-read -p "Pilih : " Input
-echo ""
+cek_ip_ke_url() {
+    IP_VALID=$(echo "$REPO_IZIN_IP" | awk '{print $3}')
+    if [[ "$CEK_IP" != "$IP_VALID" ]]; then
+        error_msg
+    fi
+}
+
+show_menu() {
+    echo -e "\033[1;35mPokemon Services :\033[0m"
+    echo -e "  \033[1;32m1.  \033[1;36mInstall Pokemon Tunneling [Paid]\033[0m"
+    echo -e "  \033[1;32m2.  \033[1;36mUpdate Pokemon Tunneling [Paid]\033[0m"
+    echo -e "  \033[1;32m3.  \033[1;36mUpdate Dependencies [Free]\033[0m"
+    echo -e "  \033[1;32m4.  \033[1;36mReset Root Password [Free]\033[0m"
+    echo -e "  \033[1;31m5.  Exit\033[0m"
+    echo ""
+    read -p $'\033[1;35mPilih Services [1-5] : \033[0m' Input
+    echo ""
+}
+
+show_menu
 
 case $Input in
     1)
-        wget https://raw.githubusercontent.com/remkenceng/pokemon/main/install.sh
+        cek_ip_ke_url
+        reset_screen
+        wget -q https://raw.githubusercontent.com/remkenceng/pokemon/main/install.sh
         chmod +x install.sh
-        reset
         bash install.sh
         rm -rf install.sh
         ;;
     2)
-        wget https://raw.githubusercontent.com/remkenceng/pokemon/main/proses_update.sh
+        cek_ip_ke_url
+        reset_screen
+        wget -q https://raw.githubusercontent.com/remkenceng/pokemon/main/proses_update.sh
         chmod +x proses_update.sh
-        reset
         bash proses_update.sh
         rm -rf proses_update.sh
         ;;
     3)
+        reset_screen
         sudo apt-get update -y && sudo apt-get upgrade -y
+        sleep 2
         bash pokemon.sh
         ;;
     4)
-        wget https://raw.githubusercontent.com/remkenceng/pokemon/main/root/install.sh
+        reset_screen
+        wget -q https://raw.githubusercontent.com/remkenceng/pokemon/main/root/install.sh
         chmod +x install.sh
         bash install.sh
         ;;
     5)
-        exit
+        reset_screen
+        echo -e "\033[1;35mThank you for using Pokemon Tunneling!\033[0m"
+        exit 0
         ;;
     *)
-        echo "Wrong Input Brok ..... !"
-        sleep 2
-        reset
+        reset_screen
+        echo -e "\033[1;31mInputan Tidak Sesuai ..... !\033[0m"
+        echo ""
+        read -n 1 -s -r -p "Press [Enter] : "
         bash pokemon.sh
         ;;
 esac
-
